d58b6efc5ae782291033a3301ab78844c5a9a237
unknown <raajeskumar.yoho@gmail.com>
2025-05-30 14:45:08 +0530

    Your message

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,8 @@ function App() {
   </div>
 </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+     <button className="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
