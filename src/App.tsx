1e2a2dc61f59d96bbebe16556f30b897ed45c546
Sumi-Yoho <sumiofficialyoho@gmail.com>
2025-06-03 12:43:18 +0530

    code update from main

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import './App.css'
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './pages/auth/AuthContext';
-import AppRoutes from './routes/AppRoutes';
-import { ToastContainer } from 'react-toastify';
-
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./pages/auth/AuthContext";
+import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <AuthProvider><AppRoutes />
-					<ToastContainer
-						position='top-right'
-						autoClose={3000}
-						hideProgressBar={false}
-						newestOnTop={false}
-						closeOnClick
-						rtl={false}
-						pauseOnFocusLoss
-						draggable
-						pauseOnHover
-						theme='colored'
-					/></AuthProvider>
+        <AuthProvider>
+          <AppRoutes />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </AuthProvider>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
